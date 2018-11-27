a569ed8841b5026107ad148b0dc45cd29c3edc39
f14shm4n <uraborg@gmail.com>
2018-11-27 22:12:42 +0300

    Init.

--- f14.xunit/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace f14.xunit
-{
-    public class Class1
-    {
-    }
-}
---
+++ f14.xunit/XUnitTestBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit.Abstractions;
+
+namespace f14.xunit
+{
+    public class XUnitTestBase
+    {
+        private readonly ITestOutputHelper _logger;
+
+        public XUnitTestBase(ITestOutputHelper logger)
+        {
+            _logger = logger;
+        }
+
+        public ITestOutputHelper Logger => _logger;
+    }
+}
